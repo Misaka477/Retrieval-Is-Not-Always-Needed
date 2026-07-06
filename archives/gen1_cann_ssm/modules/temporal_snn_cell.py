cb5e73a0567d60fb17bf7dd8657b646c5677742b
Aquama <rapidsound@163.com>
2026-07-07 00:11:47 +0800

    cleanup: consolidate archives into archive/, remove deprecated scripts, stop tracking archive/ from git

--- archives/gen1_cann_ssm/modules/temporal_snn_cell.py
+++
@@ -1,5 +0,0 @@
-"""
-Re-export from rina package for backward compatibility.
-Old scripts that do `from modules.temporal_snn_cell import ...` still work.
-"""
-from rina import TemporalSNNCell, TemporalSNNModel
